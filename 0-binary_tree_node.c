4a0516ac9ef821ec19ace5345fd3d2ba9f32930c
iamsammysamuels <iamsammysamuels@gmail.com>
2022-11-14 21:58:14 -0800

    New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a new binary tree node
+ * @parent: pointer to the parent of the binary tree
+ * @value: data of the new tree
+ *
+ * Return: pointer to the root node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *temp = NULL;
+
+    temp = malloc(sizeof(binary_tree_t));
+    if (!temp)
+        return NULL;
+
+    temp->parent = parent;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->n = value;
+    return (temp);
+}
